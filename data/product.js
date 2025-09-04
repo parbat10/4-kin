d18b9ea364ca68e0f4aa6a5e9445105f2b6c2b47
parbat10 <parbatlimbu139@gmail.com>
2025-09-04 17:17:47 +0545

    practicing

---
+++ data/product.js
@@ -0,0 +1,78 @@
+let products = [
+  {
+    image : "./images/headset.jpeg",
+    name : "Gaming Headset Pro",
+    ratings : "#",
+    price : "4000",
+  },{
+    image : "./images/headset.jpeg",
+    name : "",
+    ratings : "",
+    price : "",
+  },{
+    image : "./images/headset.jpeg",
+    name : "",
+    ratings : "",
+    price : "",
+  },{
+    image : "./images/headset.jpeg",
+    name : "",
+    ratings : "",
+    price : "",
+  },{
+    image : "./images/headset.jpeg",
+    name : "",
+    ratings : "",
+    price : "",
+  },{
+    image : "./images/phone.png",
+    name : "",
+    ratings : "",
+    price : "",
+  },{
+    image : "",
+    name : "",
+    ratings : "",
+    price : "",
+  },,{
+    image : "",
+    name : "",
+    ratings : "",
+    price : "",
+  },,{
+    image : "",
+    name : "",
+    ratings : "",
+    price : "",
+  },,{
+    image : "",
+    name : "",
+    ratings : "",
+    price : "",
+  },,{
+    image : "",
+    name : "",
+    ratings : "",
+    price : "",
+  },,{
+    image : "",
+    name : "",
+    ratings : "",
+    price : "",
+  },,{
+    image : "",
+    name : "",
+    ratings : "",
+    price : "",
+  },,{
+    image : "",
+    name : "",
+    ratings : "",
+    price : "",
+  },,{
+    image : "",
+    name : "",
+    ratings : "",
+    price : "",
+  },
+]
